620ea7a66cd85d74305f9f3431152f73cdfac9d9
Mico Rauma <Micorauma05@gmail.com>
2022-11-01 10:48:54 +0200

    Update php-harjoitus-03.php

--- php-harjoitus-03.php
+++ php-harjoitus-03.php
@@ -74,16 +74,96 @@ if($a < $b === true){
 } else {
     echo "True";
 }
-?>
-
-<?php
-$a = 5;
-$b = 8;
 
 if($b < $a === true){
     echo  "False";
 } else {
     echo "True";
+}
+?>
+
+<h1> Harjoitus 11 ja 12 </h1>
+
+<?php
+$automerkki = array("Toyota"=>"bZ4X", "Volvo"=>"V50", "Mercedes-Benz"=>"C", "Tesla"=>"Roadster", "Kia"=>"XCeed");
+print_r($automerkki);
+?>
+
+<h1> Harjoitus 13 </h1>
+<?php
+$kello = 2;
+//$aamu = range(6, 11);
+//$päivä = range(12, 16);
+//$ilta = range(17, 22);
+//$yö = range(23, 5);
+
+
+if($kello > 5 && $kello < 12 === true) {
+    echo "Nyt On Aamu.";
+}   elseif($kello > 11 && $kello < 17 === true) {
+    echo "Nyt on päivä";
+}   elseif($kello > 16 && $kello < 23 === true) {
+    echo "Nyt on ilta";
+}   elseif($kello > 22) {
+    echo "Nyt on yö";
+}   elseif($kello > 0 && $kello < 5 === true) {
+    echo "Nyt on yö";
+}
+?>
+
+<h1> Harjoitus 14 </h1>
+
+<?php
+$ihmiset = ["Jaakko", "Pate", "Viljami", "Mico", "Tarmo"];
+$taulukonpituus = count($ihmiset);
+$i = 0;
+
+while($i < $taulukonpituus)
+{
+	echo $ihmiset[$i].
+    "<br>";
+	$i++;
+}
+?>
+
+<h1> Harjoitus 15 </h1>
+<?php
+
+$kello = "16";
+
+switch ($kello) {
+  case ($kello > 11 && $kello < 17 === true):
+    echo "Nyt on päivä";
+    break;
+  case ($kello > 5 && $kello < 12 === true):
+    echo "Nyt on aamu";
+    break;
+  case ($kello > 16 && $kello < 23 === true):
+    echo "Nyt on ilta";
+    break;
+  case ($kello > 22 === true):
+    echo "Nyt on yö";
+    break;
+  case ($kello > 0 && $kello < 5 === true):
+    echo "Nyt on yö";
+    break;
+}
+?>
+
+<h1> Harjoitus 16 </h1>
+
+<?php  
+for ($numero = 0; $numero <= 70; $numero+=7) {
+  echo "Seitsemän kertotaulu: $numero <br>";
+}
+?>
+
+<h1> Harjoitus 17 </h1>
+
+<?php
+function naytaViesti() {
+    echo "Tervehdys funktiosta!";
+    naytaViesti();
 }
 ?>
 
